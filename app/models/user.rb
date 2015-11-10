aeea35b1f58f67ab98ced54e2392d4ed2c736ed7
Brit Butler <redline6561@gmail.com>
2015-11-10 14:13:01 -0500

    And avatars are now a go!

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   has_secure_password
-  has_attached_file :avatar
+  has_attached_file :avatar, styles: { thumb: "150x150>" }
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
