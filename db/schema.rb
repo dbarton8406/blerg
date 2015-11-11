a25255deaf912b919649003875604a523006892c
Brit Butler <redline6561@gmail.com>
2015-11-11 09:41:25 -0500

    Add modified schema from postgres switch.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102155156) do
+ActiveRecord::Schema.define(version: 20151110151042) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -45,10 +45,14 @@ ActiveRecord::Schema.define(version: 20151102155156) do
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
-    t.string   "email",           null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",               null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "password_digest"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
